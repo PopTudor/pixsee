49943e7ae6e868b509a4418f8155b9ca6e1f6474
Pop Tudor <tudor08pop@yahoo.com>
2016-01-23 16:55:25 +0200

    entry

---
+++ app/src/main/java/com/marked/vifo/activity/EntryActivity.kt
@@ -0,0 +1,42 @@
+package com.marked.vifo.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.preference.PreferenceManager
+import android.support.v7.app.AppCompatActivity
+
+import com.crashlytics.android.Crashlytics
+import com.marked.vifo.extra.GCMConstants
+
+import io.fabric.sdk.android.Fabric
+
+
+class EntryActivity : AppCompatActivity() {
+	private var mUserRegistered: Boolean = false
+	internal val fabric = Fabric.Builder(this)
+			.kits(Crashlytics())
+			.debuggable(true) // TODO: 13-Dec-15 disable this
+			.build()
+
+	override
+	fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		Fabric.with(fabric)
+
+		mUserRegistered = PreferenceManager.getDefaultSharedPreferences(this).getBoolean(GCMConstants.SENT_TOKEN_TO_SERVER, false)
+	}
+
+	override
+	fun onResume() {
+		super.onResume()
+		val intent: Intent
+		if (mUserRegistered) {
+			intent = Intent(this, ContactListActivity::class.java)
+		} else {
+			intent = Intent(this, EntryAuthActivity::class.java)
+		}
+		startActivity(intent)
+		finish()
+	}
+
+}
