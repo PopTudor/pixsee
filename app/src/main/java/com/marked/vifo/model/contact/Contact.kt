bd39fbcd8ab231b4074459338c88555ffc931436
Pop Tudor <tudor08pop@yahoo.com>
2016-02-11 14:05:36 +0200

    updated contact

--- app/src/main/java/com/marked/vifo/model/contact/Contact.kt
+++ app/src/main/java/com/marked/vifo/model/contact/Contact.kt
@@ -10,29 +10,34 @@ import java.util.*
 /**
  * Created by Tudor Pop on 28-Nov-15.
  */
-class Contact(id: String, name: String, token: String) : Comparator<Contact>, Comparable<Contact>, Parcelable {
-    var id: String
-    var name: String
-    var token: String
-
-    init {
-        this.id = id
-        this.name = name
-        this.token = token
-    }
+data class Contact(var id: String,var name: String,var email: String,var token: String) : Comparator<Contact>, Comparable<Contact>, Parcelable {
+    constructor(id: String, name: String, token: String) : this(id, name, "", token)
 
     companion object {
-        val CREATOR = object : Parcelable.Creator<Contact> {
+        @JvmField @Suppress("unused")
+        val CREATOR:Parcelable.Creator<Contact> = object : Parcelable.Creator<Contact> {
             override fun createFromParcel(parcelIn: Parcel) = Contact(parcelIn)
             override fun newArray(size: Int): Array<Contact?> = arrayOfNulls(size)
         };
 
-        private fun Contact(parcelIn: Parcel): Contact {
+        protected fun Contact(parcelIn: Parcel): Contact {
             var id = parcelIn.readString()
             var name = parcelIn.readString()
+            var email = parcelIn.readString()
             var token = parcelIn.readString()
-            return Contact(id, name, token)
+            return Contact(id, name, email, token)
         }
+    }
+
+    override fun describeContents(): Int {
+        return 0;
+    }
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeString(id);
+        dest.writeString(name);
+        dest.writeString(email);
+        dest.writeString(token);
     }
 
     override fun compareTo(other: Contact): Int {
@@ -43,11 +48,11 @@ class Contact(id: String, name: String, token: String) : Comparator<Contact>, Co
         return lhs.id.compareTo(rhs.id);
     }
 
-    override fun equals(other: Any?) = if (other is Contact && id.equals(other.id)) true else false
-
     fun toJSON(): JSONObject {
         val jsonObject = JSONObject();
         jsonObject.put(UserConstants.ID, id);
+        jsonObject.put(UserConstants.NAME, name);
+        jsonObject.put(UserConstants.EMAIL, email);
         jsonObject.put(UserConstants.TOKEN, token);
         return jsonObject;
     }
@@ -55,6 +60,8 @@ class Contact(id: String, name: String, token: String) : Comparator<Contact>, Co
     fun toContentValues(): ContentValues {
         val values = ContentValues();
         values.put(UserConstants.ID, id);
+        values.put(UserConstants.NAME, name);
+        values.put(UserConstants.EMAIL, email);
         values.put(UserConstants.TOKEN, token);
         return values;
     }
@@ -62,23 +69,7 @@ class Contact(id: String, name: String, token: String) : Comparator<Contact>, Co
     fun fromJSON(json: JSONObject) {
         id = json.getString(UserConstants.ID);
         name = json.getString(UserConstants.NAME);
+        email = json.getString(UserConstants.EMAIL);
         token = json.getString(UserConstants.TOKEN);
-    }
-
-    override fun describeContents(): Int {
-        return 0;
-    }
-
-    override fun writeToParcel(dest: Parcel, flags: Int) {
-        dest.writeString(id);
-        dest.writeString(name);
-        dest.writeString(token);
-    }
-
-    override fun hashCode(): Int {
-        var result = id.hashCode()
-        result += 31 * result + name.hashCode()
-        result += 31 * result + token.hashCode()
-        return result
     }
 }
