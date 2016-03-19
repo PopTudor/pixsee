b47dbcf86049c6ab1b295a249774b069eca37185
Pop Tudor <tudor08pop@yahoo.com>
2016-03-19 12:00:10 +0200

    Friends Dagger

--- app/src/main/java/com/marked/pixsee/friends/di/FriendsComponent.java
+++ app/src/main/java/com/marked/pixsee/friends/di/FriendsComponent.java
@@ -8,6 +8,8 @@ import dagger.Component;
 
 /**
  * Created by Tudor Pop on 19-Mar-16.
+ * Because this componend depends on appcomponent, you have to add the appcomponent to the builder
+ * DaggerFriendsComponent.builder().appComponent(appcomponent).friendModule(new FriendModule(this)).build().inject(this);
  */
 @PerFragment
 @Component(modules = {FriendModule.class}, dependencies = {AppComponent.class})
