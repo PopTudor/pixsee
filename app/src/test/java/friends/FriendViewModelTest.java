2e3b83baf785edaabfac148064fc0d350809851f
Pop Tudor <tudor08pop@yahoo.com>
2016-03-30 12:58:59 +0300

    Test FriendViewModel

--- app/src/test/java/friends/FriendViewModelTest.java
+++ app/src/test/java/friends/FriendViewModelTest.java
@@ -1,65 +1,70 @@
 package friends;
 
-import android.test.mock.MockContext;
-
 import com.google.common.collect.Lists;
-import com.marked.pixsee.friends.FriendsContract;
 import com.marked.pixsee.data.User;
-import com.marked.pixsee.friends.data.FriendRepository;
+import com.marked.pixsee.data.repository.Repository;
+import com.marked.pixsee.data.repository.SQLSpecification;
+import com.marked.pixsee.friends.FriendViewModel;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.Mockito.anyInt;
+import rx.Observable;
+
+import static junit.framework.Assert.assertEquals;
+import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by Tudor Pop on 16-Mar-16.
- * Unit tests for the implementation of {@link FriendPresenter}
+ * Unit tests for the implementation of {@link com.marked.pixsee.friends.FriendViewModel}
  */
 public class FriendViewModelTest {
 	private static List<User> FRIENDS = Lists
-			.newArrayList(new User("user", "xyz", "xyz@emai.com", "asd",null), new User("user1", "zxc", "zxc@emai.com", "zxc",null));
+			.newArrayList(new User("user1", "xyz", "xyz@emai.com", "asd"), new User("user2", "zxc", "zxc@emai.com", "zxc"),
+			              new User("user3", "zxc", "zxc@emai.com", "zxc"), new User("user4", "zxc", "zxc@emai.com", "zxc"));
 
-	private static List<User> EMPTY_NOTES = new ArrayList<>(0);
 
 	@Mock
-	private FriendRepository mFriendRepository;
+	Repository<User>             mRepository;
+	@Mock
+	FriendViewModel.DataListener mDataListener;
 
+	FriendViewModel mViewModel;
 
-
-	MockContext context = new MockContext();
 	@Before
-	public void setupNotesPresenter() {
-		// Mockito has a very convenient way to inject mocks by using the @Mock annotation. To
-		// inject the mocks in the test the initMocks method needs to be called.
+	public void setup() {
 		MockitoAnnotations.initMocks(this);
-//		Mockito.when(context.getApplicationContext()).thenReturn(new Application());
-
-//		mFriendRepository = ;
-		// Get a reference to the class under test
+		mViewModel = new FriendViewModel(mRepository);
+		mViewModel.setDataListener(mDataListener);
 	}
 
 	@Test
-	public void testLoadFriends() throws Exception {
-
-		verify(mFriendRepository).loadMore(FRIENDS.size());
-		Assert.assertEquals(mFriendRepository.size(), FRIENDS.size());
+	public void testLoadFriendsForced() throws Exception {
+		when(mRepository.query(any(SQLSpecification.class))).thenReturn(Observable.from(FRIENDS).take(2).toList());
+		mViewModel.loadFriends(true, 2);
+		verify(mDataListener).onFriendsLoaded(FRIENDS.subList(0, 2), 0, FRIENDS.size() / 2); /* callback got triggered */
+		assertEquals(2, mViewModel.getSize());
 	}
 
-	@Test
-	public void testLoadFriends1() throws Exception {
-
-	}
 
 	@Test
-	public void testOpenFriendDetailUI() throws Exception {
-
+	public void testLoadFriendsShouldAppend() throws Exception {
+		when(mRepository.query(any(SQLSpecification.class))).thenReturn(Observable.just(FRIENDS.subList(0, 2)))
+		                                                    .thenReturn(Observable.just(FRIENDS));
+		mViewModel.loadFriends(true, 2);
+		
+		verify(mDataListener).onFriendsLoaded(FRIENDS.subList(0, 2), 0, 2); /* load only 2 items */
+		assertEquals(2, mViewModel.getSize());
+		/* add friends to the already loaded list */
+		mViewModel.loadFriends(false, 2);
+		// load another 2 items, starting with an offset of 2
+		verify(mDataListener).onFriendsLoaded(FRIENDS.subList(2, 4), 2, 2);
+		assertEquals(4, mViewModel.getSize());
 	}
 }
